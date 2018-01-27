5e1058dae96a35e63d347318e09be11b7986b3e9
jalapenosbud <jalapenosbud@gmail.com>
2018-01-27 21:29:57 +0100

    virker ikke

--- Lily/src/lily/VendeSpilController.java
+++ Lily/src/lily/VendeSpilController.java
@@ -23,6 +23,8 @@ import javafx.scene.layout.GridPane;
  */
 public class VendeSpilController implements Initializable {
 
+    public String clickedImageView1;
+    public String clickedImageView2;
     public GridPane gridPane = new GridPane();
     private Boolean setCardFlipped = false;
     private String[] saveClicks = new String[2];
@@ -122,7 +124,7 @@ public class VendeSpilController implements Initializable {
                     .toString());
             green1.setImage(image);
             
-            System.out.println("red has been clicked");
+            System.out.println("green has been clicked");
         }
         if(ev.getPickResult().getIntersectedNode().getId().equals("green2"))
         {
@@ -131,10 +133,6 @@ public class VendeSpilController implements Initializable {
             green2.setImage(image);
             System.out.println("green has been clicked");
         }
-        
-        
-        
-        
     }
     
     @FXML
@@ -152,32 +150,35 @@ public class VendeSpilController implements Initializable {
         if(setCardFlipped)
         {
             
-            if(mCount == 2)
+            if(mCount == 1)
             {
+                clickedImageView1 = event.getPickResult().getIntersectedNode().getId();
+                System.out.println("clicked this: " + clickedImageView1);
                 CheckArray(saveClicks,mCount,event.getPickResult().getIntersectedNode().getId(), null);
             }
-            else if(mCount == 3)
+            else if(mCount == 2)
             {
+                clickedImageView2 = event.getPickResult().getIntersectedNode().getId();
+                System.out.println("clicked this: " + clickedImageView2);
                 CheckArray(saveClicks,mCount, saveClicks[0], event.getPickResult().getIntersectedNode().getId());
+                //check bricks
+                EvalBricks(clickedImageView1, clickedImageView2);
             }
+            
+            
 
 
             for (int i = 0; i < saveClicks.length; i++) {
                 System.out.println(Arrays.toString(saveClicks));
             }
-            
-            if(!DoesBricksMatch())
-            {
-                
-            }
-            
-            
         }
         
         //String tempstr = event.getPickResult().getIntersectedNode().getId();
         
         System.out.println(event.getPickResult().getIntersectedNode().getId());
     }
+    
+    
     
     private void ClearData()
     {
@@ -189,39 +190,39 @@ public class VendeSpilController implements Initializable {
         mCount = 0;
     }
     
-    private Boolean DoesBricksMatch()
-    {
-        //                              0       1       2       3           4       5       6       7
-        String[] colors = new String[]{"red1","red2","orange1","orange2","green1","green2","blue1","blue2"};
-        //red 1 and 2 matches
-        if(EvalBricks(colors[0],colors[1]))
-        {
-            return true;
-        }
-        //orange 1 and 2
-        if(EvalBricks(colors[2],colors[3]))
-        {
-            return true;
-        }
-        //green1 and 2
-        if(EvalBricks(colors[4],colors[5]))
-        {
-            return true;
-        }
-        //blue 1 and 2
-        if(EvalBricks(colors[6],colors[7]))
-        {
-            return true;
-        }
-        return false;
-    }
-    
     private Boolean EvalBricks(String matchBrick1, String matchBrick2)
     {
-        if(matchBrick1.equals(matchBrick2))
+       //                              0       1       2       3           4       5       6       7
+        String[] colors = new String[]{"red1","red2","orange1","orange2","green1","green2","blue1","blue2"};
+        
+        //matches red1 and red2
+        if(matchBrick1.equals(colors[0]) && matchBrick2.equals(colors[1]))
         {
+            System.out.println("its a match!!!!");
             return true;
         }
+        
+        //matches orange1 and orange2
+        if(matchBrick1.equals(colors[2]) && matchBrick2.equals(colors[3]))
+        {
+            System.out.println("its a match!!!!");
+            return true;
+        }
+        
+        //matches green1 and green2
+        if(matchBrick1.equals(colors[4]) && matchBrick2.equals(colors[5]))
+        {
+            System.out.println("its a match!!!!");
+            return true;
+        }
+        
+        //matches blue1 and blue2
+        if(matchBrick1.equals(colors[6]) && matchBrick2.equals(colors[7]))
+        {
+            System.out.println("its a match!!!!");
+            return true;
+        }
+        System.out.println("NO MATCH!!!");
         return false;
     }
     
