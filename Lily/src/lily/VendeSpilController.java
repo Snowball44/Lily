15adfb23dcf38cd362cd12b05522f1e0876259d7
jalapenosbud <jalapenosbud@gmail.com>
2018-01-27 16:51:34 +0100

    nu virker vendespil nogenlunde

--- Lily/src/lily/VendeSpilController.java
+++ Lily/src/lily/VendeSpilController.java
@@ -6,6 +6,7 @@
 package lily;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -22,6 +23,9 @@ import javafx.scene.layout.GridPane;
 public class VendeSpilController implements Initializable {
 
     public GridPane gridPane = new GridPane();
+    private Boolean setCardFlipped = false;
+    private String[] saveClicks = new String[2];
+    private Integer mCount = 0;
     /**
      * Initializes the controller class.
      */
@@ -32,12 +36,47 @@ public class VendeSpilController implements Initializable {
         
     }
     
-    
+    void CheckArray(String[] arrs,int cnt, String input, String input2)
+    {
+        System.out.println("count before: " + cnt);
+        if(cnt == 1)
+        {
+            //sæt det første input til arr 0 element
+            arrs[0] = input;
+        }
+        else if(cnt == 2)
+        {
+            arrs[1] = input2;
+        }
+        System.out.println("count after: " + cnt);
+    }
     
     @FXML
     private void clickKort(MouseEvent event) 
-    {    
+    {   
+        if(!setCardFlipped)
+        {
+            setCardFlipped = true;
+        }
         
+        mCount++;
+        
+        if(mCount <= 1)
+        {
+             
+            
+            CheckArray(saveClicks,mCount,event.getPickResult().getIntersectedNode().getId(), null);
+        }
+        else if(mCount >= 1 && mCount <= 2)
+        {
+            CheckArray(saveClicks,mCount, saveClicks[0], event.getPickResult().getIntersectedNode().getId());
+        }
+        
+        
+        for (int i = 0; i < saveClicks.length; i++) {
+            System.out.println(Arrays.toString(saveClicks));
+        }
+        //String tempstr = event.getPickResult().getIntersectedNode().getId();
         
         
         
@@ -45,6 +84,14 @@ public class VendeSpilController implements Initializable {
         System.out.println(event.getPickResult().getIntersectedNode().getId());
     }
     
+    private Boolean EvalBricks(String matchBrick1, String matchBrick2)
+    {
+        if(matchBrick1.equals(matchBrick2))
+        {
+            return true;
+        }
+        return false;
+    }
     
     
 }
