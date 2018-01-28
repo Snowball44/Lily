d56a39e18b1519b0d723935a1d5a1d27d9c4cbbd
Zuzu <Zuzu@192.168.123.51>
2018-01-28 03:06:30 +0100

    fjernelse af fejl

--- Lily/src/Game_data/Mess.java
+++ Lily/src/Game_data/Mess.java
@@ -72,7 +72,4 @@ public class Mess {
         return seeMission.getCompleted();
     }
 
-    void setWon(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
